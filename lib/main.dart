701421f5c03858ae1044ee60db52d2788d3a64d1
jimin <jiminseo0307@gmail.com>
2025-01-14 16:51:46 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff1F1F1F),
+      ),
+    );
+  }
+}
